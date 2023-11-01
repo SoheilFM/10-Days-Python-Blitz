933ade432345e61715564fd0a3f15b46425f75c1
SoheilFM <soheil.farid1999@yahoo.com>
2023-11-01 23:43:00 +0330

    Add python basics

---
+++ Day-1/pythonBasics.py
@@ -0,0 +1,7 @@
+# Variable  
+name="Soheil" #Assign name to Soheil
+#Snake case is used in python when you have a variable name more than one Word.
+#Camel Case means Starting each new word with a capital letter except the first Like cookieCount
+cookie_count=12 
+cookie_count=cookie_count - 2
+cookie_count=cookie_count  + 10
