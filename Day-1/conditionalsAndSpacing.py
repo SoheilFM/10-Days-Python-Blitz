29326d58d3e5bb9faa88a24719a565834f3170c6
SoheilFM <soheil.farid1999@yahoo.com>
2023-11-04 23:59:03 +0330

    Add first simple if

--- Day-1/conditionalsAndSpacing.py
+++ Day-1/conditionalsAndSpacing.py
@@ -1,2 +1,6 @@
-name= input("Please Enter Your Name")
-print(name)
+name = input("Please Enter Your Name")
+#print(name)
+#What of we want to say somthing diffrent base
+#on Who the person is?
+if (name == "Soheil"):#An if Statement takes ()
+    print("Hello, Master "+ name)
