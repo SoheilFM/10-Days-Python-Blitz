787e2707a1c29185a35b264f94567329c53e2702
SoheilFM <soheil.farid1999@yahoo.com>
2023-11-04 23:41:22 +0330

    Add print data Type

--- Day-1/dataTypes.py
+++ Day-1/dataTypes.py
@@ -1,7 +1,21 @@
 #Data Types
+
 really_long_text="""I am
  the best
   version
    of
     myself"""
 print(really_long_text)
+print("=====================")
+an_integer = 1
+a_float  =3.14
+a_boolean= True
+
+really_long_text_data_type = type(really_long_text)
+print(really_long_text_data_type)
+an_integer_data_type = type(an_integer)
+print(an_integer)
+a_float_data_type = type(a_float)
+print(a_float_data_type)
+a_boolean_data_type = type(a_boolean)
+print(a_boolean_data_type)
