02dc853136ad5faad6ee7768b2ba4bb2824e4fe0
SoheilFM <soheil.farid1999@yahoo.com>
2023-11-05 01:15:45 +0330

    Add Simple Elif

--- Day-1/conditionalsAndSpacing.py
+++ Day-1/conditionalsAndSpacing.py
@@ -1,9 +1,12 @@
-name = input("Please Enter Your Name")
+name = input("Please Enter Your Name ==> ")
 #print(name)
 #What of we want to say somthing diffrent base
 #on Who the person is?
 if (name == "Soheil"):#An if Statement takes ()
     print("Hello, Master "+ name)
-    #python is an indented language!
+    #python is an indented language
+# Add Another Statement 
+elif(name=="Farid"):
+    print("Hello, Senior "+name)
 else:
-    print("Hello, Student")
+    print("Hello, Student ")
