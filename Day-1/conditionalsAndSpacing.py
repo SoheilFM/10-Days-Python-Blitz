3011e60c53df80c07daadfeec13c366a5ac05b44
SoheilFM <soheil.farid1999@yahoo.com>
2023-11-05 01:18:28 +0330

    Add comments

--- Day-1/conditionalsAndSpacing.py
+++ Day-1/conditionalsAndSpacing.py
@@ -2,10 +2,13 @@ name = input("Please Enter Your Name ==> ")
 #print(name)
 #What of we want to say somthing diffrent base
 #on Who the person is?
+
+# An if means "They are evaluated seprately
 if (name == "Soheil"):#An if Statement takes ()
     print("Hello, Master "+ name)
     #python is an indented language
 # Add Another Statement 
+# An Elif means "which one is True?"
 elif(name=="Farid"):
     print("Hello, Senior "+name)
 else:
