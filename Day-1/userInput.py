89f78a64d3ea9ba1e43e62bf70962ca966f11120
SoheilFM <soheil.farid1999@yahoo.com>
2023-11-04 23:52:32 +0330

    Add comments and name into input

--- Day-1/userInput.py
+++ Day-1/userInput.py
@@ -1,4 +1,7 @@
 #input () will PAUSE or BLOCK the program 
 #until  the user enters somthing
-name= input()
-print(name)
+name= input("what is your name ? >>") # Input is always a sting data type
+#the + sign is not a plus sign , it is called 
+#concaneate in programing.concatenation is when you
+# Stick 2 srtings together.
+print("Welcome, "+ name)
