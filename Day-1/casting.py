c7a9dd1811290ef4634af1145717f9d38f305edb
SoheilFM <soheil.farid1999@yahoo.com>
2023-11-06 00:47:23 +0330

    Add casting examples

--- Day-1/casting.py
+++ Day-1/casting.py
@@ -1,3 +1,9 @@
 #casting = Change a vaiable data type without changing the variable itself
 a_Number_as_string = "2" #string
 print (int (a_Number_as_string) + 2)
+print(a_Number_as_string + str(2))
+print(float(3))
+
+fav_number = input("What is your favorate Number? > ")
+print(type(fav_number))
+fav_number_plus_three = int(fav_number) + 3
