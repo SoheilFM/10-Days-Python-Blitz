33e3ec9eac0d8df342daa93b21e889219f38d08b
SoheilFM <soheil.farid1999@yahoo.com>
2023-11-05 23:47:31 +0330

    add codes

--- Day-1/pythonMath.py
+++ Day-1/pythonMath.py
@@ -1,2 +1,8 @@
-#Oprator- symobl /sign that can manipulate / or do somthing with and oprand
-# Oprand - is something that ann oprator affects
+#Operator- symobl /sign that can manipulate / or do somthing with and oprand
+# Operand - is something that ann operator affects
+a_number = 3+5
+############python opertator
+a_number = 5 +3.14
+# + = concatenate symbol when used with strings 
+a_string= "Soheil" + "Farid"
+nonesence = "soheil" +3 #this will break 
